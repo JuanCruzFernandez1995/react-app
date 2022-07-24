f638c4a8059d1a547d05e7238b7462e04721f5df
Juan Cruz Fernandez <juancruzfernandez1995@gmail.com>
2022-07-24 20:52:19 -0300

    Agregando component NavBar

---
+++ mi-react/src/components/NavBar.js
@@ -0,0 +1,33 @@
+function NavBar() {
+    return (
+      <div className="navBar">
+          <h1>
+              E-commerce React
+          </h1>
+          <ul>
+            <li>
+                <a href="Home">
+                  Home
+                </a>
+            </li>
+            <li>
+                <a href="Products">
+                  Products
+                </a>
+            </li>
+            <li>
+                <a href="About us">
+                    About us
+                </a>
+            </li>
+            <li>
+                <a href="Contact">
+                  Contact
+                </a>
+            </li>
+          </ul>
+      </div>
+    );
+  }
+
+  export default NavBar;
