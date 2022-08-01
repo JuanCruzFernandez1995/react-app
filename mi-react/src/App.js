24ec3db9cd79c48ad9bb5fa091a12b2173f85821
Juan Cruz Fernandez <juancruzfernandez1995@gmail.com>
2022-07-31 22:37:04 -0300

    add itemlistcontainer con greeting

--- mi-react/src/App.js
+++ mi-react/src/App.js
@@ -2,12 +2,19 @@ import './App.css';
 import ItemListContainer from "./components/ItemListContainer";
 import NavBar from './components/NavBar';
 
+
 function App() {
   return (
     <div className="App">
       <header>
         <NavBar />
       </header>
+      <main>
+        <div className="mainContainer container border mt-5">
+          <ItemListContainer greeting="Greeting" />
+          
+        </div>
+      </main>
     </div>
   );
 }
---
+++ mi-react/src/components/ItemListContainer.js
@@ -0,0 +1,7 @@
+function ItemListContainer(props){
+    return (
+        <h3 className="fw-light">{props.greeting}</h3>   
+    );
+}
+
+export default ItemListContainer;
