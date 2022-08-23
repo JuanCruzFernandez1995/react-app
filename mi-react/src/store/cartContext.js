dfff225f5d7d0762b3dec6c805bd1795df69b52a
Juan Cruz Fernandez <juancruzfernandez1995@gmail.com>
2022-08-22 23:27:35 -0300

    cartcontext function finditem y removeitem

--- mi-react/src/store/cartContext.js
+++ mi-react/src/store/cartContext.js
@@ -6,7 +6,7 @@ export function CartProvider ({children}) {
     const [cart, setCart] = useState([]);
 
     let copyCart = [...cart];
-
+    //Agregar un producto, previamente valida con isInCart, si existe solo actualiza la cantidad, sino lo agrega.
     function addToCart(item, quantity){
         if(!isInCart(item.id)){
             copyCart.push( {...item, quantity} );
@@ -26,17 +26,29 @@ export function CartProvider ({children}) {
             setCart(cartUpdate)
         }
     }
-
+    //Validacion si un producto ya existe en el cart por su ID
     function isInCart (id){
         return (cart.some((itemInCart) => itemInCart.id === id))
     }
-
-    function removeAll (){
+    //Eliminar todos los productos del cart
+    function clearCart (){
         setCart([])
+    }
+    //Eliminar un producto del cart
+    function removeItem (item){
+        const itemRemove = findItem(item.id);
+        const indexItem = copyCart.indexOf(itemRemove);
+        copyCart.splice(indexItem, 1);
+        setCart(copyCart);
+        console.log(copyCart);
+    }
+    //Buscar un item por su ID
+    function findItem(id){
+        return (copyCart.find(item => item.id === parseInt(id)))
     }
 
     return (
-        <cartContext.Provider value={ {cart, addToCart, removeAll, isInCart} }>
+        <cartContext.Provider value={ {cart, addToCart, clearCart, removeItem, isInCart} }>
             {children}
         </cartContext.Provider>
     )
